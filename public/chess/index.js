a97d51cc6a75dc61ffc193404c72b731229d580d
Daniel Zheng <daniel.zheng@barnebys.com>
2020-10-11 19:35:00 +0200

    draw board

--- public/chess/index.js
+++ public/chess/index.js
@@ -3,36 +3,47 @@ const g = canvas.getContext("2d");
 
 let oldTime;
 
-const state = "♖♘♗♔♕♗♘♖♙♙♙♙♙♙♙♙................................♟︎♟︎♟︎♟︎♟︎♟︎♟︎♟︎♜♞♝♚♛♝♞♜";
+const state = [
+    '♜','♞','♝','♚','♛','♝','♞','♜',
+    '♟︎','♟︎','♟︎','♟︎','♟︎','♟︎','♟︎','♟︎',
+    ' ',' ',' ',' ',' ',' ',' ',' ',
+    ' ',' ',' ',' ',' ',' ',' ',' ',
+    ' ',' ',' ',' ',' ',' ',' ',' ',
+    ' ',' ',' ',' ',' ',' ',' ',' ',
+    '♙','♙','♙','♙','♙','♙','♙','♙',
+    '♖','♘','♗','♔','♕','♗','♘','♖',
+];
 
 const draw = (deltaTime) => {
-    g.fillStyle = "#FFFFFF";
+    g.fillStyle = "white";
     g.fillRect(0, 0, 600, 600);
 
     const s = 50;
-    g.fillStyle = "#000000";
 
     for(let y=0; y<8; y++){
         for(let x=0; x<8; x++){
         
-            if(x + y % 2 == 0){
-                g.fillStyle = "#000000";
+            if((x + y) % 2 == 0){
+                g.fillStyle = "gray";
             }
             else{
-                g.fillStyle = "#FFFFFF";
+                g.fillStyle = "white";
             }
 
             g.fillRect(x * s, y * s, s, s);
         }
     }
 
-    g.fillStyle = "#000000";
+    g.fillStyle = "black";
     g.font = "30px Arial";
+    g.textBaseline = 'middle'; 
+    g.textAlign = 'center'; 
 
     for(let y=0; y<8; y++){
         for(let x=0; x<8; x++){
             const symbol = state[y * 8 + x];
-            g.fillText(symbol, x * s, y * s);
+            console.log(symbol);
+            g.fillText(symbol, x * s + s / 2, y * s + s / 2);
         }
     }
 };
