04db10e95bf7385f34b4dc1ef3abfd64d310372e
Daniel Zheng <daniel.zheng@barnebys.com>
2020-10-11 20:19:01 +0200

    dark mode

--- public/chess/index.js
+++ public/chess/index.js
@@ -3,7 +3,7 @@ const g = canvas.getContext("2d");
 
 let oldTime;
 
-const startState = [
+let state = [
     '♜','♞','♝','♚','♛','♝','♞','♜',
     '♟︎','♟︎','♟︎','♟︎','♟︎','♟︎','♟︎','♟︎',
     ' ',' ',' ',' ',' ',' ',' ',' ',
@@ -12,9 +12,10 @@ const startState = [
     ' ',' ',' ',' ',' ',' ',' ',' ',
     '♙','♙','♙','♙','♙','♙','♙','♙',
     '♖','♘','♗','♔','♕','♗','♘','♖',
+    'W',
 ];
 
-const draw = (deltaTime) => {
+const render = async (deltaTime) => {
     g.fillStyle = "white";
     g.fillRect(0, 0, 600, 600);
 
@@ -52,29 +53,41 @@ const draw = (deltaTime) => {
     drawSymbol('2', 8, 6);
     drawSymbol('1', 8, 7);
 
-    drawSymbol('A', 0, 8);
-    drawSymbol('B', 1, 8);
-    drawSymbol('C', 2, 8);
-    drawSymbol('D', 3, 8);
-    drawSymbol('E', 4, 8);
-    drawSymbol('F', 5, 8);
-    drawSymbol('G', 6, 8);
-    drawSymbol('H', 7, 8);
+    drawSymbol('a', 0, 8);
+    drawSymbol('b', 1, 8);
+    drawSymbol('c', 2, 8);
+    drawSymbol('d', 3, 8);
+    drawSymbol('e', 4, 8);
+    drawSymbol('f', 5, 8);
+    drawSymbol('g', 6, 8);
+    drawSymbol('h', 7, 8);
 
     for(let y=0; y<8; y++){
         for(let x=0; x<8; x++){
-            drawSymbol(startState[y * 8 + x], x, y);
+            drawSymbol(state[y * 8 + x], x, y);
         }
     }
 };
 
-const gameLoop = (newTime) => {
+const move = (state, source, target) => {
+    console.log(source, target);
+    console.log(source[0], source.charCodeAt(0) - 'a'.charCodeAt(0));
+    console.log(source[1], 7 - (source.charCodeAt(1) - '1'.charCodeAt(0)) );
+    return [...state];
+};
+
+state = move(state, "e2", "e4");
+state = move(state, "e7", "e6");
+state = move(state, "f2", "f4");
+state = move(state, "f2", "f4");
+
+const gameLoop = async (newTime) => {
     const deltaTime = newTime - oldTime;
     
-    draw(deltaTime);
+    await render(deltaTime);
     
     oldTime = newTime;
-    // window.requestAnimationFrame(gameLoop);
+    window.requestAnimationFrame(gameLoop);
 };
 
 window.requestAnimationFrame(gameLoop);
