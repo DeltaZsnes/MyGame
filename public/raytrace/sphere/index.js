61f810da950f3ba5b56a8103caf530c05ba8555a
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-28 12:44:32 +0200

    working ray tracing

--- public/raytrace/sphere/index.js
+++ public/raytrace/sphere/index.js
@@ -11,6 +11,8 @@ let positionData = null;
 let sphereData = null;
 let sphereLocation = null;
 
+let oldTime = null;
+
 const vsSource = `
 attribute vec4 position;
 varying vec2 st;
@@ -22,14 +24,49 @@ void main(void) {
 `;
 
 const fsSource = `
+#define sphereCount 4
+#define focalLength 4
+#define rayCountMax 1
+
 precision mediump float;
-uniform vec4 sphere[4];
+uniform vec4 sphere[sphereCount];
+uniform vec3 cameraPosition[1];
+uniform mat3 cameraRotation[1];
 varying vec2 st;
 
 void main(void) {
+    vec3 rayPosition;
+	vec3 rayDirection;
+	int rayCount = 0;
+
     vec4 background = vec4(0.1, 0.2, 0.3, 1.0);
-    vec4 xxx = vec4(st.s, st.t, 0, 1);
-    gl_FragColor = background + xxx;
+    vec4 rayColor = background;
+
+    rayPosition = cameraPosition[0];
+	rayDirection = cameraRotation[0] * vec3(st, focalLength);
+    rayCount++;
+
+    for(int sphereIndex = 0; sphereIndex < sphereCount; sphereIndex++)
+	{
+        vec3 spherePosition = sphere[sphereIndex].xyz;
+        float sphereRadius = sphere[sphereIndex].w;
+        vec3 sphereColor = vec3(mod(spherePosition.x, 1.0), mod(spherePosition.y, 1.0), mod(spherePosition.z, 1.0), 1.0);
+        float a = dot(rayDirection, rayDirection);
+        vec3 f = rayPosition - spherePosition;
+		float b = dot(2.0 * rayDirection, f);
+		float c = dot(f, f) - (sphereRadius * sphereRadius);
+		float d = (b * b) - 4.0 * a * c;
+		float e = sqrt(d);
+		float t0 = (-b + e) / (2.0 * a);
+		float t1 = (-b - e) / (2.0 * a);
+
+        // to avoid branching multiply by the max
+        // thus we technically always add all sphere colors
+        float m = max(d, 0.0);
+        rayColor = rayColor + vec4(1, 0, 0, 1) * m;
+    }
+
+    gl_FragColor = rayColor;
 }
 `;
 
@@ -37,7 +74,7 @@ const init = () => {
     vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vsSource);
     gl.compileShader(vertexShader);
-  
+
     if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
         throw gl.getShaderInfoLog(vertexShader);
     }
@@ -45,7 +82,7 @@ const init = () => {
     fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fsSource);
     gl.compileShader(fragmentShader);
-  
+
     if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
         throw gl.getShaderInfoLog(fragmentShader);
     }
@@ -57,6 +94,8 @@ const init = () => {
 
     positionLocation = gl.getAttribLocation(shaderProgram, "position");
     sphereLocation = gl.getUniformLocation(shaderProgram, "sphere");
+    cameraPositionLocation = gl.getUniformLocation(shaderProgram, "cameraPosition");
+    cameraRotationLocation = gl.getUniformLocation(shaderProgram, "cameraRotation");
 
     positionData = new Float32Array([
         -1.0, -1.0, 0.0,
@@ -75,40 +114,36 @@ const init = () => {
         +3.0, 0.0, +2.0, 1.0,
     ]);
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
+    cameraPosition = vec3.create();
+    cameraRotation = mat3.create();
 
-    // const fieldOfView = 45 * Math.PI / 180; // in radians
-    // const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    // const zNear = 0.1;
-    // const zFar = 100.0;
-    // const projectionMatrix = mat4.create();
-
-    // // note: glmatrix.js always has the first argument
-    // // as the destination to receive the result.
-    // mat4.perspective(projectionMatrix,
-    //     fieldOfView,
-    //     aspect,
-    //     zNear,
-    //     zFar);
-
-    // // Set the drawing position to the "identity" point, which is
-    // // the center of the scene.
-    // const modelViewMatrix = mat4.create();
-
-    // // Now move the drawing position a bit to where we want to
-    // // start drawing the square.
-
-    // mat4.translate(modelViewMatrix, // destination matrix
-    //     modelViewMatrix, // matrix to translate
-    //     [0.0, 0.0, -6.0]); // amount to translate
+    document.addEventListener('keypress', (e) => {
+        switch (e.key) {
+            case "w":
+                vec3.add(cameraPosition, cameraPosition, vec3.fromValues(0, 0, +1));
+                break;
+            case "s":
+                vec3.add(cameraPosition, cameraPosition, vec3.fromValues(0, 0, -1));
+                break;
+            case "a":
+                vec3.add(cameraPosition, cameraPosition, vec3.fromValues(-1, 0, 0));
+                break;
+            case "d":
+                vec3.add(cameraPosition, cameraPosition, vec3.fromValues(+1, 0, 0));
+                break;
+            case "q":
+                break;
+            case "r":
+                break;
+        }
+    });
 };
 
 const render = () => {
+    let newTime = performance.now();
+    // console.log(1000.0/(newTime - oldTime));
+    oldTime = newTime;
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -116,12 +151,16 @@ const render = () => {
     // gl.cullFace(gl.BACK);
 
     gl.useProgram(shaderProgram);
-    
+
     gl.enableVertexAttribArray(positionLocation);
     gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, positionData.length / 3);
 
     gl.uniform4fv(sphereLocation, sphereData);
+    gl.uniform3fv(cameraPositionLocation, cameraPosition);
+    gl.uniformMatrix3fv(cameraRotationLocation, false, cameraRotation);
+
+    window.requestAnimationFrame(render);
 };
 
 init();
