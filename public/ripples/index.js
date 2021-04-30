1273abd9b3df5478a5947d175668e0d7ab845bac
Daniel Zheng <daniel.zheng@barnebys.com>
2021-04-30 16:15:48 +0200

    added mouse events

--- public/ripples/index.js
+++ public/ripples/index.js
@@ -125,7 +125,7 @@ const loadTexture = (imageUrl) => {
 
             resolve(texture);
         };
-        
+
         image.src = imageUrl;
     });
 }
@@ -155,14 +155,20 @@ const init = async () => {
     gl.bindTexture(gl.TEXTURE_2D, waterTexture);
     gl.texImage2D(gl.TEXTURE_2D, level, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.generateMipmap(gl.TEXTURE_2D);
 
     const frameBuffer = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
 
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, waterTexture, level);
+
+    document.addEventListener('mousedown', (e) => {
+        console.log(e);
+    });
+    document.addEventListener('mouseup', (e) => {
+    });
+    document.addEventListener('mousemove', (e) => {
+    });
 };
 
 const render = () => {
@@ -177,7 +183,7 @@ const render = () => {
         gl.activeTexture(gl.TEXTURE0);
 
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.useProgram(touchProgram);
 
@@ -196,7 +202,7 @@ const render = () => {
         gl.activeTexture(gl.TEXTURE1);
 
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.useProgram(waterProgram);
         gl.uniform1i(waterTextureLocation, 0);
